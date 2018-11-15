852befe4c78c4fa47f8e8b89ee511d7d80f4af20
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-11-15 09:49:06 -0500

    Commit inicial

---
+++ AppVectores/AppVectores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVectores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("control de versiones");
+            Console.ReadKey();
+        }
+    }
+}
